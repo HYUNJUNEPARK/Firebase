076419c41912560458d83e7a380bbc75beb3d2a3
HYUNJUNEPARK <june2ac4dev@gmail.com>
2022-04-15 08:53:52 +0900

    init

---
+++ app/src/main/java/com/example/firebasetutorial/activity/StoreActivity.kt
@@ -0,0 +1,13 @@
+package com.example.firebasetutorial.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.firebasetutorial.R
+
+class StoreActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_store)
+    }
+}
