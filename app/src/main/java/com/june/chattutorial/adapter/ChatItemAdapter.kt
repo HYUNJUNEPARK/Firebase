e38c2f7984c254a9ad0b89847d88ba2885d928ab
HYUNJUNEPARK <june2ac4dev@gmail.com>
2022-05-02 17:09:46 +0900

    getItemViewType

--- app/src/main/java/com/june/chattutorial/adapter/ChatItemAdapter.kt
+++ app/src/main/java/com/june/chattutorial/adapter/ChatItemAdapter.kt
@@ -9,42 +9,48 @@ import androidx.recyclerview.widget.RecyclerView
 import com.june.chattutorial.databinding.ItemPartnerUserChatBinding
 import com.june.chattutorial.firebase.FBVal.Companion.TAG
 import com.june.chattutorial.firebase.FBVal.Companion.currentUser
+import com.june.chattutorial.key.ViewType.Companion.CURRENT_USER_MESSAGE
+import com.june.chattutorial.key.ViewType.Companion.PARTNER_USER_MESSAGE
 import com.june.chattutorial.model.ChatItemModel
 
-class ChatItemAdapter : ListAdapter<ChatItemModel, ChatItemAdapter.PartnerHolder>(diffUtil) {
-
-
-    inner class PartnerHolder(private val binding: ItemPartnerUserChatBinding): RecyclerView.ViewHolder(binding.root) {
+class ChatItemAdapter : ListAdapter<ChatItemModel, ChatItemAdapter.ViewHolder>(diffUtil) {
+    inner class ViewHolder(private val binding: ItemPartnerUserChatBinding): RecyclerView.ViewHolder(binding.root) {
         fun bind(chatItem: ChatItemModel) {
             binding.messageTextView.text = chatItem.message
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PartnerHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val binding = ItemPartnerUserChatBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return PartnerHolder(binding)
+
+        return when (viewType) {
+            CURRENT_USER_MESSAGE -> {
+                Log.d(TAG, "onCreateViewHolder: 내가 보낸 메세지")
+                ViewHolder(binding)
+            }
+            PARTNER_USER_MESSAGE  -> {
+                Log.d(TAG, "onCreateViewHolder: 남이 보낸 메세지")
+                ViewHolder(binding)
+            }
+            else -> {
+                Log.d(TAG, "--------------")
+                ViewHolder(binding)
+            }
+        }
     }
 
-    override fun onBindViewHolder(holder: PartnerHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind(currentList[position])
     }
 
-//    override fun getItemViewType(position: Int): Int {
-//        return super.getItemViewType(position)
-//
-//        val tt: ChatItemModel = getItem(position)
-//
-//        //내가 보낸 메시지
-//        return if (tt.senderId == currentUser?.uid) {
-//            0
-//            Log.d(TAG, "getItemViewType: 내 메세지")
-//        //다른 사람 보낸 메세지
-//        } else {
-//            1
-//            Log.d(TAG, "getItemViewType: 다른 사람 메세지")
-//        }
-//
-//    }
+    override fun getItemViewType(position: Int): Int {
+        val message = currentList[position]
+        return if (message.senderId == currentUser?.uid) {
+            CURRENT_USER_MESSAGE
+        } else {
+            PARTNER_USER_MESSAGE
+        }
+    }
 
     companion object {
         val diffUtil = object : DiffUtil.ItemCallback<ChatItemModel>() {
